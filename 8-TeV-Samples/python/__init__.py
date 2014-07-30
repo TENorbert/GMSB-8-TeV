885b657d22178cdbdeb0b5ebd8465ece1eb500e1
TENorbert <delsubdue@gmail.com>
2014-07-30 17:25:38 -0500

    mGMSB config + fragments

---
+++ 8-TeV-Samples/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/GMSB-8-TeV/8-TeV-Samples/',1)[0])+'/cfipython/slc5_amd64_gcc472/GMSB-8-TeV/8-TeV-Samples')
